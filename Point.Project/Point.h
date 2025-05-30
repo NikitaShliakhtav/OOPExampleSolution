2e767941739d883a9ddffa61f766ae509121d4a1
shakal2004 <bl4ckr0s3@mail.ru>
2025-05-30 18:52:21 +0300

    class Student

---
+++ Point.Project/Point.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>#include <string>
+using namespace std;
+
+class Point2D {
+public:
+	int x;
+	int y;
+
+	string getQuarter() {
+		string msg = "in IV-quarted-";
+
+		if (x == 0 && y == 0) {
+			msg = "in the center";
+		}
+		else if (y == 0) {
+			msg = x > 0 ? "on the x-right-line" 
+				: "on the x-left-line";
+				}
+		else if (x == 0) {
+			msg = y > 0 ? "on the y-up-line"
+				: "on the y-down-line";
+		}
+		else if (x > 0 && y > 0){
+			msg = "in I-quarter";
+			}
+		else if (x < 0 && y > 0) {
+			msg = "in II-quarter";
+		}
+		else if (x < 0 && y < 0) {
+			msg = "in III-quarter";
+		}
+
+		return msg;
+	}
+};
