c63a0f944cb939300ca507852c1f1c845ecaeac8
shakal2004 <bl4ckr0s3@mail.ru>
2025-06-06 18:47:36 +0300

    update

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -1,6 +1,10 @@
+﻿
 #pragma once
 #include "main.h"
 
+// KISS & DRY
+
+// entity-class
 class Student {
 public:
 	string firstname;
@@ -10,16 +14,53 @@ public:
 	char gender;
 	bool alive;
 	double mark;
+	int size;
+	string* subjects;
 
-	Student() {
+	// default-constructor or constructor without arguments
+	Student() : Student("no name", "no surname") {
 		//cout << "default-constructor" << endl;
-		firstname = "no name";
-		surname = "no surname";
-		age = 0;
-		_class = 0;
-		gender = 'm';
-		alive = false;
-		mark = 4;
+	}
+
+	// constructor with arguments 
+	Student(string firstname, string surname) : firstname(firstname),
+		surname(surname), age(0), _class(0), gender('m'), alive(false),
+		mark(0), size(0), subjects(nullptr) {
+		//cout << "constructor with arguments" << endl;	
+	}
+
+	Student(string firstname, string surname, int age) : Student(firstname,
+		surname, age, 0, 'm', true, 4, 0, nullptr) {
+		//cout << "constructor with arguments" << endl;
+	}
+
+	// canonical constructor 
+	Student(string firstname, string surname, int age, int _class,
+		char gender, bool alive, double mark, int size, string* subjects) {
+		//cout << "canonical constructor" << endl;
+		this->firstname = firstname;
+		this->surname = surname;
+		this->age = age;
+		this->_class = _class;
+		this->gender = gender;
+		this->alive = alive;
+		this->mark = mark;
+		this->size = size;
+		this->subjects = subjects;
+	}
+
+	// copy-constructor 
+	Student(const Student& student) : Student(student.firstname,
+		student.surname, student.age, student._class, student.gender,
+		student.alive, student.mark, student.size, student.subjects) {
+		//cout << "canonical constructor" << endl;
+	}
+
+	~Student() {
+		// cout << "destructor" << endl;
+		if (subjects != nullptr) {
+			delete[] subjects;
+		}
 	}
 
 	string toString() {
