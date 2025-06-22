9cb4609aae4cd5d3231788699d62f825f3ce9f3e
shakal2004 <bl4ckr0s3@mail.ru>
2025-06-22 18:58:00 +0300

    add queue

---
+++ ADSProject/Queue.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "main.h"
+
+
+class Queue
+{
+private:
+	int* numbers;
+	int size;
+public:
+	Queue() : numbers(nullptr), size(0) { }
+	~Queue();
+
+	void enqueue(int number);	
+	int dequeue();		
+	int head();
+	bool isEmpty();
+	void clear();
+	int getSize();
+
+	string toString();
+};
